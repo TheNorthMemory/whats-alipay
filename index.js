2aa75af899057778ea298d7a9eba6229c4e7d92b
zhangjunjun <thenorthmemory@dingtalk.com>
2021-04-04 12:38:10 +0800

    [WIP] eslint index.js, lib/alipay.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-module.exports = require('./lib/alipay')
-module.exports.default = module.exports
-module.exports.Alipay = module.exports
-module.exports.Rsa = require('./lib/rsa')
-module.exports.Aes = require('./lib/aes')
-module.exports.AesCbc = module.exports.Aes.AesCbc
-module.exports.Form = require('./lib/form')
-module.exports.Formatter = require('./lib/formatter')
-module.exports.Decorator = require('./lib/decorator')
-module.exports.Helpers = require('./lib/helpers')
+module.exports = require('./lib/alipay');
+
+module.exports.default = module.exports;
+module.exports.Alipay = module.exports;
+module.exports.Rsa = require('./lib/rsa');
+module.exports.Aes = require('./lib/aes');
+
+module.exports.AesCbc = module.exports.Aes.AesCbc;
+module.exports.Form = require('./lib/form');
+module.exports.Formatter = require('./lib/formatter');
+module.exports.Decorator = require('./lib/decorator');
+module.exports.Helpers = require('./lib/helpers');
