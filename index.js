493f9691e484133db95e0dfe7a963bb332f06305
James ZHANG <thenorthmemory@dingtalk.com>
2021-08-02 21:08:57 +0800

    renaming form -> multipart

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ module.exports.Rsa = require('./lib/rsa');
 module.exports.Aes = require('./lib/aes');
 
 module.exports.AesCbc = module.exports.Aes.AesCbc;
-module.exports.Form = require('./lib/form');
+module.exports.Multipart = require('./lib/multipart');
+
+module.exports.Form = module.exports.Multipart;
 module.exports.Formatter = require('./lib/formatter');
 module.exports.Decorator = require('./lib/decorator');
 module.exports.Helpers = require('./lib/helpers');
